9ab060dfd2572b135fbfa95b9aa9fb44bd3c57d2
SuicaLondon <66697085+SuicaLondon@users.noreply.github.com>
2024-09-03 16:29:36 +0100

    chore: add comment

--- src/app/videos-ssr/loading.tsx
+++ src/app/videos-ssr/loading.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import RotateLeftIcon from '@mui/icons-material/RotateLeft'
 
 export default function Loading() {
+	/// It can be improved by replacing icon
 	return (
 		<div className="flex flex-1 items-center justify-center">
 			<RotateLeftIcon className="h-28 w-28 animate-spin" />
